aee97f12b7380263db1f49b2d290a382562abeae
Carlos Henrique <62728022+CarlosHenrique13@users.noreply.github.com>
2022-06-01 13:45:59 -0300

    reload files

--- site antigo/js/img.js
+++
@@ -1,8 +0,0 @@
-function Zom(id){
-    var img = document.getElementById(id);
-    if (img.style.width === "80%"){
-        img.style.width = "20%"
-    }else{
-        img.style.width = "80%"
-    }
-}
